80b67aaa7ae6534be80aba79c0c13b99ce571f74
Legolas <razatr@yandex.ru>
2020-05-28 15:47:31 +0300

    Updates

---
+++ precache-manifest.de9636cf7146d8fa4d2d049c7cf6dcf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2509b427dd7e9972a5c8b13c96669ba3",
+    "url": "/open-bank-sub-list/index.html"
+  },
+  {
+    "revision": "3217460a5f564b8bd506",
+    "url": "/open-bank-sub-list/static/js/2.6f3a79f6.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/open-bank-sub-list/static/js/2.6f3a79f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35945d489f390b93d80",
+    "url": "/open-bank-sub-list/static/js/main.839ea334.chunk.js"
+  },
+  {
+    "revision": "3185b6325c77a172eca5",
+    "url": "/open-bank-sub-list/static/js/runtime-main.1fb9c4ff.js"
+  },
+  {
+    "revision": "f0ec220827f4bc47e0e90cb05d9aa432",
+    "url": "/open-bank-sub-list/static/media/Header_logo.f0ec2208.svg"
+  },
+  {
+    "revision": "75e1c5bc78848df5d798e7c2fc6d9186",
+    "url": "/open-bank-sub-list/static/media/Lock.75e1c5bc.svg"
+  }
+]);
